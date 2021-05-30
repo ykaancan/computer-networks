c490ea58023282d109dce91a7b29558c4902c0ec
Kaan Can Yıldırım <59798767+ykaancan@users.noreply.github.com>
2021-05-30 17:41:40 +0300

    Add files via upload
    word letter game via tcp

---
+++ TCPClient/TCPClient/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace TCPClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.wordPool = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.turnLabel = new System.Windows.Forms.Label();
+            this.timerLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 2;
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Location = new System.Drawing.Point(23, 30);
+            this.txtInfo.Multiline = true;
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.ReadOnly = true;
+            this.txtInfo.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtInfo.Size = new System.Drawing.Size(302, 224);
+            this.txtInfo.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(71, 283);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(302, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.AutoSize = true;
+            this.txtMessage.Location = new System.Drawing.Point(12, 286);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(53, 13);
+            this.txtMessage.TabIndex = 5;
+            this.txtMessage.Text = "Message:";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(217, 320);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(298, 320);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 7;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // wordPool
+            // 
+            this.wordPool.FormattingEnabled = true;
+            this.wordPool.Location = new System.Drawing.Point(340, 30);
+            this.wordPool.Name = "wordPool";
+            this.wordPool.Size = new System.Drawing.Size(129, 225);
+            this.wordPool.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(373, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Word Pool";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(166, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Chat";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // turnLabel
+            // 
+            this.turnLabel.AutoSize = true;
+            this.turnLabel.Location = new System.Drawing.Point(12, 325);
+            this.turnLabel.Name = "turnLabel";
+            this.turnLabel.Size = new System.Drawing.Size(74, 13);
+            this.turnLabel.TabIndex = 16;
+            this.turnLabel.Text = "Player1\'s Turn";
+            // 
+            // timerLabel
+            // 
+            this.timerLabel.AutoSize = true;
+            this.timerLabel.Location = new System.Drawing.Point(112, 325);
+            this.timerLabel.Name = "timerLabel";
+            this.timerLabel.Size = new System.Drawing.Size(35, 13);
+            this.timerLabel.TabIndex = 17;
+            this.timerLabel.Text = "0s left";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 355);
+            this.Controls.Add(this.timerLabel);
+            this.Controls.Add(this.turnLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.wordPool);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtInfo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSend);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Player2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtInfo;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label txtMessage;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.ListBox wordPool;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label turnLabel;
+        private System.Windows.Forms.Label timerLabel;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ TCPClient/TCPClient/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SimpleTcp;
+
+namespace TCPClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SimpleTcpClient client;
+
+        string last_word;
+        char last_letter;
+        char before_last_letter;
+        char first_letter;
+        char after_first_letter;
+
+        int timer = 10;
+        int turn = 2;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new("127.0.0.1:9000");
+            client.Events.Connected += Events_Connected;
+            client.Events.Disconnected += Events_Disconnected;
+            client.Events.DataReceived += Events_DataReceived;
+            btnSend.Enabled = false;
+            timerLabel.Visible = false;
+        }
+
+        private void Events_DataReceived(object sender, DataReceivedEventArgs e)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                txtInfo.Text += $"Player1: {Encoding.UTF8.GetString(e.Data)}{Environment.NewLine}";
+                wordPool.Items.Add(Encoding.UTF8.GetString(e.Data));
+                btnSend.Enabled = true;
+                turn = 2;
+                turnLabel.Text = $"Player{turn}'s Turn...";
+                timer1.Start();
+                timer = 10;
+                timerLabel.Visible = true;
+                
+            });
+            
+        }
+
+        private void Events_Disconnected(object sender, ClientDisconnectedEventArgs e)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                txtInfo.Text += $"Server disconnected. {Environment.NewLine}";
+            });
+            
+        }
+
+        private void Events_Connected(object sender, ClientConnectedEventArgs e)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                txtInfo.Text += $"Server connected. {Environment.NewLine}";
+
+            });
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (client.IsConnected)
+            {
+                if (!string.IsNullOrEmpty(textBox3.Text))
+                {
+                    if (wordPool.Items.Contains(textBox3.Text) || wordPool.Items.Contains(textBox3.Text))
+                    {
+                        string containsError = "The word you wrote have been sent before. Try a different one.";
+                        MessageBox.Show(containsError);
+                    }
+                    else
+                    {
+                        last_word = wordPool.Items[wordPool.Items.Count - 1].ToString(); // burasi calismiyor buna bak
+                        char[] chars = last_word.ToCharArray();
+                        last_letter = chars[chars.Length - 1];
+                        before_last_letter = chars[chars.Length - 2];
+                        string lastTwoLetters = before_last_letter.ToString() + last_letter.ToString();
+
+                        char[] charTxtMessage = textBox3.Text.ToCharArray();
+                        first_letter = charTxtMessage[0];
+                        after_first_letter = charTxtMessage[1];
+                        string firstTwoLetters = first_letter.ToString() + after_first_letter.ToString();
+
+                        if (lastTwoLetters == firstTwoLetters)
+                        {
+                            client.Send(textBox3.Text);
+                            wordPool.Items.Add(textBox3.Text);
+                            txtInfo.Text += $"Player2: {textBox3.Text}{Environment.NewLine}";
+                            textBox3.Text = string.Empty;
+                            btnSend.Enabled = false;
+                            turn = 1;
+                            turnLabel.Text = $"Player{turn}'s Turn...";
+                            timer = 10;
+                        }
+                        else
+                        {
+                            string wrongWord = "The word you wrote doesn't satisfy the game rules. Try a different one.";
+                            MessageBox.Show(wrongWord);
+                        }
+                        
+                    }
+                }
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                client.Connect();
+/*              btnSend.Enabled = true;
+*/              btnConnect.Enabled = false;
+            }
+            catch(Exception exc)
+            {
+                MessageBox.Show(exc.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer--;
+            timerLabel.Text = $"{timer.ToString()}s Left.";
+            if (timer == 0)
+            {
+                timer1.Stop();
+                if (turn == 1)
+                {
+                    string timeIsUp = "TIME IS UP. PLAYER2 LOST";
+                    MessageBox.Show(timeIsUp);
+                }
+                else if (turn == 2)
+                {
+                    string timeIsUp = "TIME IS UP. PLAYER1 WON";
+                    MessageBox.Show(timeIsUp);
+                }
+
+
+
+            }
+        }
+    }
+}
